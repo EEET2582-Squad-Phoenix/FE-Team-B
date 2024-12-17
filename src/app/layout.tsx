566a2245a147e05f474fad14becbc3a70fffeb82
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2024-12-17 18:12:34 +0700

    Set up Redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import StoreProvider from "./StoreProvider";
 
 export const metadata: Metadata = {
   title: "Charitan for Admin",
@@ -13,11 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main>{children}</main>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
