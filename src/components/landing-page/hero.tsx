23361da12f49c32cf3da64a32a128bf479a8814e
EvanBao2506 <dinhgiabao430@gmail.com>
2025-01-11 10:30:31 +0700

    Adjust layer for hero section

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -6,14 +6,20 @@ export default function Hero() {
             alt="background"
             className="absolute inset-0 object-cover w-full h-full"
             />
+
             <div className="absolute inset-0 bg-black/50"></div>
-            <div className="relative z-10 flex flex-col items-start justify-start h-full text-left text-white p-40">
-            <h1 className="text-5xl font-bold">Make a Difference Today</h1>
-            <p className="mt-4 text-base max-w-lg">
-                Join a global community of change-makers supporting impactful
-                projects. Discover meaningful causes, donate securely, and create
-                lasting change.
-            </p>
+            
+            <div className="relative container mx-auto px-4 h-full flex flex-col items-start justify-center">
+                <div className="max-w-2xl pt-20">
+                    <h1 className="text-5xl font-bold text-white">
+                        Make a Difference Today
+                    </h1>
+                    <p className="mt-4 text-lg text-white/90 max-w-lg">
+                        Join a global community of change-makers supporting impactful
+                        projects. Discover meaningful causes, donate securely, and create
+                        lasting change.
+                    </p>
+                </div>
             </div>
         </section>
     );
