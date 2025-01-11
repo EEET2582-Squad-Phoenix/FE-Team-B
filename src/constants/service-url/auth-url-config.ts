1805c8b2bb00d827fbc7a1a451dfb306598ab8af
EvanBao2506 <dinhgiabao430@gmail.com>
2025-01-11 12:39:40 +0700

    Update urlurl

--- src/constants/service-url/auth-url-config.ts
+++ src/constants/service-url/auth-url-config.ts
@@ -3,6 +3,8 @@ import { HOST_URL_B } from "./app-url-config";
 export const AUTH_SERVICE_URL = `${HOST_URL_B}/auth`;
 
 export const AUTH_SIGNIN_URL = `${AUTH_SERVICE_URL}/login`;
+export const AUTH_SIGNOUT_URL = `${AUTH_SERVICE_URL}/logout`;
+export const AUTH_GET_ME_URL = `${AUTH_SERVICE_URL}/get-me`;
 export const AUTH_CHECK_EMAIL_URL = `${AUTH_SERVICE_URL}/check-email`;
 export const AUTH_REGISTER_URL = `${AUTH_SERVICE_URL}/register`;
 
