6622630fcb59909183e368dd00c81195b0f5613e
Thu Nguyen <101914949+thu-ngx@users.noreply.github.com>
2025-01-14 17:24:10 +0700

    Update app-url-config.ts

--- src/constants/service-url/app-url-config.ts
+++ src/constants/service-url/app-url-config.ts
@@ -4,6 +4,6 @@ export const HOST_URL_A = "http://localhost:" + HOST_PORT_A;
 const HOST_PORT_B = 8080;
 export const HOST_URL_B = "http://localhost:" + HOST_PORT_B;
 
-const HOST_PORT_A_FE = 8080;
+const HOST_PORT_A_FE = 3001;
 export const DONATE_AS_GUEST_URL =
   "http://localhost:" + HOST_PORT_A_FE + "/guest";
