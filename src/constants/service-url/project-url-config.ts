49d76d64d9b13824fe321359d0b340bf5dc83a79
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-01-14 14:41:57 +0700

    Remove unused url config

--- src/constants/service-url/project-url-config.ts
+++ src/constants/service-url/project-url-config.ts
@@ -9,18 +9,10 @@ export const PROJECT_PAGINATION_URL = `${PROJECT_SERVICE_URL_A}/allProjects`;
 export const PROJECT_UPDATE_URL = `${PROJECT_SERVICE_URL_A}/admin/update`;
 export const PROJECT_APPROVE_URL = `${PROJECT_SERVICE_URL_A}/approve`;
 export const PROJECT_RECOVER_URL = `${PROJECT_SERVICE_URL_A}/recoverProject`;
-// export const PROJECT_HALT_URL = `${PROJECT_SERVICE_URL_A}/halt`;
-// export const PROJECT_RESUME_URL = `${PROJECT_SERVICE_URL_A}/unhalt`;
 export const PROJECT_TOGGLE_HALTED_URL = `${PROJECT_SERVICE_URL_A}/toggleHalt`;
 
 export const PROJECT_TOGGLE_HIGHLIGHTED_URL = `${PROJECT_SERVICE_URL_A}/toggleHighlighted`;
 export const PROJECT_DEACTIVATE_URL = `${PROJECT_SERVICE_URL_A}/deactivate`;
 export const PROJECT_RESTORE_URL = `${PROJECT_SERVICE_URL_A}/restore`;
 
-// export const PROJECT_CHECK_HIGHLIGHTED_GLOBAL_URL = `${PROJECT_SERVICE_URL_A}/checkHighlightedGlobal`;
-// export const PROJECT_CHECK_HIGHLIGHTED_REGIONAL_URL = `${PROJECT_SERVICE_URL_A}/checkHighlightedRegional`;
-
-export const PROJECT_DELETE_URL = `${PROJECT_SERVICE_URL_A}/adminDelete`; // used
-
-// export const PROJECT_SERVICE_URL_B = `${HOST_URL_B}/charity/projects`;
-// export const PROJECT_APPROVE_URL = `${PROJECT_SERVICE_URL_B}/approve`;
+export const PROJECT_DELETE_URL = `${PROJECT_SERVICE_URL_A}/adminDelete`;
